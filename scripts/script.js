19769964d40bc22b07b4ee301d84670304e76138
Liza <liz4chernyshova@yandex.ru>
2021-03-19 12:37:31 +0300

    fix: исправить ошибки

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+let popup = document.querySelector('.popup');
+let formElement = popup.querySelector('.popup__container');
+let btnRedactor = document.querySelector('.profile__redactor-btn');
+let closePopup = popup.querySelector('.popup__close-btn');
+let nameInput = popup.querySelector('input[name="heading"]');
+let jobInput = popup.querySelector('input[name="subheading"]');
+let userName = document.querySelector('.profile__info-name'); 
+let userDesc = document.querySelector('.profile__info-description'); 
+
+btnRedactor.addEventListener('click', showClick);
+closePopup.addEventListener('click', closeClick);
+formElement.addEventListener('submit', formSubmitHandler);
+
+function showClick() {
+   popup.classList.add('popup_opened');
+   nameInput.value = userName.textContent;
+   jobInput.value = userDesc.textContent;
+}
+function closeClick() {
+   popup.classList.remove('popup_opened');
+}
+function formSubmitHandler (evt) {
+   evt.preventDefault();
+   userName.textContent = nameInput.value;
+   userDesc.textContent = jobInput.value;
+   closeClick();
+}
+
