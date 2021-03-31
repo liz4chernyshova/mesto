a252eb7e1229d0b5f484db6469739fb8aabcf883
Liza <liz4chernyshova@yandex.ru>
2021-03-31 13:52:23 +0300

    style: добавить картинки

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 let popup = document.querySelector('.popup');
 let popupAdd = document.querySelector('.popup-add');
 let popupPhoto = document.querySelector('.popup-photo');
+console.log(popupPhoto);
 let photoImage = popupPhoto.querySelector('.popup-photo__image');
-let form = popupAdd.querySelector('.form');
+let formAdd = popupAdd.querySelector('.form');
 let btnPhoto = popupPhoto.querySelector('.popup-photo__button');
 let photoTitle = popupPhoto.querySelector('.popup-photo__title');
 let formElement = popup.querySelector('.popup__container');
@@ -17,38 +18,38 @@ let userDesc = document.querySelector('.profile__info-description');
 let btnAdd = document.querySelector('.profile__submit-btn');
 let btnAddClose = popupAdd.querySelector('.popup-add__close-btn');
 const photoElements = document.querySelector('.photo-elements');
-console.log(photoElements);
 
 const initialCards = [
   {
-    name: 'Канада',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
     name: 'США',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    link: 'https://raw.githubusercontent.com/liz4chernyshova/mesto/main/images/elements/Chicago.jpg'
   },
   {
     name: 'Греция',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    link: 'https://raw.githubusercontent.com/liz4chernyshova/mesto/main/images/elements/Greece.jpg'
+  },
+  {
+    name: 'Канада',
+    link: 'https://raw.githubusercontent.com/liz4chernyshova/mesto/main/images/elements/Canada.jpg'
   },
   {
     name: 'Норвегия',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    link: 'https://raw.githubusercontent.com/liz4chernyshova/mesto/main/images/elements/Norway.jpg'
   },
   {
     name: 'Япония',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    link: 'https://raw.githubusercontent.com/liz4chernyshova/mesto/main/images/elements/Tokio.jpg'
   },
   {
     name: 'Швейцария',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    link: 'https://raw.githubusercontent.com/liz4chernyshova/mesto/main/images/elements/Swizerland.jpg'
   }
 ];
 
 btnRedactor.addEventListener('click', showClick);
 closePopup.addEventListener('click', closeClick);
 formElement.addEventListener('submit', formSubmitHandler);
+formAdd.addEventListener('submit', addElementCard);
 btnAdd.addEventListener('click', addClick);
 btnAddClose.addEventListener('click', closeAdd);
 btnPhoto.addEventListener('click', () => closePhoto());
@@ -74,11 +75,6 @@ function closeAdd() {
   popupAdd.classList.remove('popup_opened');  //работает
 }
 
-function deleteElement() {
-  const listItem = elementDel.closest('.photo-element');
-  listItem.remove();
-}
-
 function showPhoto() {
   popupPhoto.classList.add('popup_opened');
 }
@@ -91,15 +87,26 @@ function addElement(card) {
   const templateElements = document.querySelector('#template-element').content;
   const newElement = templateElements.querySelector('.photo-element').cloneNode(true);
   const elementPicture = newElement.querySelector('.photo-element__picture');
-  const elementContent = newElement.querySelector('.photo-element__content');
   const elementTitle = newElement.querySelector('.photo-element__title');
-  const elementDel = newElement.querySelector('.photo-element__delete-btn');
+  const elementDelete = newElement.querySelector('.photo-element__delete-btn');
+  const elementLike = newElement.querySelector('.photo-element__like');
 
   elementTitle.textContent = card.name;
-  elementPicture.alt = card.name;
+  elementPicture.alt = card.alt;
   elementPicture.src = card.link;
+  elementLike.addEventListener('click', likeElement);
+  elementDelete.addEventListener('click', deleteElement);
+  elementPicture.addEventListener('click', () => openCard(card.link, card.name));
   return newElement;
  }
+
+ function likeElement(evt) {
+  evt.target.classList.toggle('photo-element__like_active');  //работает
+}
+
+function deleteElement(evt) {
+  evt.target.closest('.photo-element').remove();  //работает
+}
  
  function openElement() {
    const inCards = initialCards.map(card => {
@@ -115,8 +122,9 @@ openElement();
 function addElementCard(evt) {
   evt.preventDefault();
   const elementCard = addElement({name: nameCard.value, link: linkCard.value});
-  photoElements.prepend(...elementCard);
+  photoElements.prepend(elementCard);
   closeAdd(popupAdd);
+  formAdd.reset();
 }
 
 function openCard(link, alt) {
